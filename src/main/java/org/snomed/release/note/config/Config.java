80adde7a3f33350dd3e02c6799a91275a4b11d58
Elena Ilyukhina <eil@ihtsdo.org>
2021-12-22 13:15:41 +0000

    Placeholder Config.java

---
+++ src/main/java/org/snomed/release/note/config/Config.java
@@ -0,0 +1,11 @@
+package org.snomed.release.note.config;
+
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.PropertySource;
+
+@Configuration
+@EnableConfigurationProperties
+@PropertySource(value = "classpath:application.properties", encoding = "UTF-8")
+public class Config {
+}
