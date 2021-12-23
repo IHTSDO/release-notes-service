29e768ffdfe84f309c9c91a75967c00397dab159
CoderMChu <mch@ihtsdo.org>
2021-12-23 14:43:27 +0000

    RNMS-20 Set up testConcainer

--- src/test/java/org/snomed/release/note/AbstractTest.java
+++ src/test/java/org/snomed/release/note/AbstractTest.java
@@ -1,27 +1,29 @@
 package org.snomed.release.note;
 
+import org.elasticsearch.client.RestHighLevelClient;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.data.elasticsearch.core.ElasticsearchRestTemplate;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
 import org.testcontainers.elasticsearch.ElasticsearchContainer;
 import org.testcontainers.junit.jupiter.Testcontainers;
+
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
 @Testcontainers
 @ExtendWith(SpringExtension.class)
 @ContextConfiguration(classes = {TestConfig.class})
 abstract class AbstractTest {
 	@Autowired
-	ElasticsearchRestTemplate elasticsearchRestTemplate;
+	RestHighLevelClient elasticsearchClient;
 
 	private static ElasticsearchContainer elasticsearchContainer = TestConfig.getElasticsearchContainerInstance();
 
 	@BeforeAll
 	static void setUp() {
-		if (!elasticsearchContainer.isRunning()) {
-			elasticsearchContainer.start();
+		if (!TestConfig.useLocalElasticsearch) {
+			assertTrue(TestConfig.getElasticsearchContainerInstance().isRunning(), "Test container is not running");
 		}
 	}
 }
