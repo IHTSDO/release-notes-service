5819a68734eb1001961e94ef587ef458be54ebac
CoderMChu <mch@ihtsdo.org>
2021-12-23 14:43:27 +0000

    RNMS-20 Set up testConcainer

--- src/main/java/org/snomed/release/note/config/Config.java
+++ src/main/java/org/snomed/release/note/config/Config.java
@@ -1,10 +1,10 @@
 package org.snomed.release.note.config;
 
+import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
-import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.PropertySource;
 
-@Configuration
+@SpringBootApplication
 @EnableConfigurationProperties
 @PropertySource(value = "classpath:application.properties", encoding = "UTF-8")
 public class Config extends ElasticsearchConfig {
--- src/main/java/org/snomed/release/note/config/ElasticsearchConfig.java
+++ src/main/java/org/snomed/release/note/config/ElasticsearchConfig.java
@@ -1,15 +1,17 @@
 package org.snomed.release.note.config;
 
 import org.elasticsearch.client.RestHighLevelClient;
+import org.springframework.context.annotation.Bean;
 import org.springframework.data.elasticsearch.client.ClientConfiguration;
 import org.springframework.data.elasticsearch.client.RestClients;
 import org.springframework.data.elasticsearch.config.AbstractElasticsearchConfiguration;
 
 public class ElasticsearchConfig extends AbstractElasticsearchConfiguration {
 
-	@Override
-	public RestHighLevelClient elasticsearchClient() {
-		// TODO externalize config for elasticsearch url, index prefix and shard configurations etc.
-		return RestClients.create(ClientConfiguration.localhost()).rest();
-	}
+    @Override
+    @Bean
+    public RestHighLevelClient elasticsearchClient() {
+        // TODO externalize config for elasticsearch url, index prefix and shard configurations etc.
+        return RestClients.create(ClientConfiguration.localhost()).rest();
+    }
 }
