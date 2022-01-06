64b1bba7de2b9fe6af2711f5a864b199eed91515
CoderMChu <mch@ihtsdo.org>
2022-01-06 11:25:06 +0000

    Update elasticsearch version to 7.15.2

--- src/test/java/org/snomed/release/note/TestConfig.java
+++ src/test/java/org/snomed/release/note/TestConfig.java
@@ -13,10 +13,10 @@ import org.testcontainers.junit.jupiter.Container;
 
 @TestConfiguration
 public class TestConfig extends Config {
-	private static final String ELASTIC_SEARCH_SERVER_VERSION = "7.15.1";
+	private static final String ELASTIC_SEARCH_SERVER_VERSION = "7.16.2";
 
 	// set it to true to use local instance instead of test container
-	static final boolean useLocalElasticsearch = true;
+	static final boolean useLocalElasticsearch = false;
 
 	private static final Logger LOGGER = LoggerFactory.getLogger(TestConfig.class);
 
@@ -31,7 +31,7 @@ public class TestConfig extends Config {
 				LOGGER.error("No docker client available to run integration tests.");
 				LOGGER.info("Integration tests use the TestContainers framework.(https://www.testcontainers.org)");
 				LOGGER.info("TestContainers framework requires docker to be installed.(https://www.testcontainers.org/supported_docker_environment)");
-				LOGGER.info("You can download docker(2.3.0.4) via (https://docs.docker.com/get-docker)");
+				LOGGER.info("You can download docker via (https://docs.docker.com/get-docker)");
 				System.exit(-1);
 			}
 			elasticsearchContainer = new TestElasticsearchContainer();
