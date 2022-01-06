2029294d09c3b89c0d4754a3a90fff1352d86230
CoderMChu <mch@ihtsdo.org>
2022-01-06 16:54:28 +0000

    Add swagger ui

--- src/main/java/org/snomed/release/note/config/SecurityConfig.java
+++ src/main/java/org/snomed/release/note/config/SecurityConfig.java
@@ -1,22 +1,45 @@
 package org.snomed.release.note.config;
 
+import org.ihtsdo.sso.integration.RequestHeaderAuthenticationDecorator;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
-import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.web.authentication.www.BasicAuthenticationFilter;
+import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spring.web.plugins.Docket;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
+
+import static com.google.common.base.Predicates.not;
+import static springfox.documentation.builders.PathSelectors.regex;
 
 @Configuration
 @EnableWebSecurity
+@EnableSwagger2
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
 
 	@Override
-	protected void configure(HttpSecurity http) throws Exception {
-		http.sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS);
-		http.csrf().disable();
+	public void configure(HttpSecurity http) throws Exception {
 		http.authorizeRequests()
-				.antMatchers("/**")
-				.permitAll();
+				.antMatchers("/swagger-ui.html",
+						"/swagger-resources/**",
+						"/v2/api-docs",
+						"/webjars/springfox-swagger-ui/**").permitAll()
+				.anyRequest().authenticated()
+				.and().httpBasic();
+		http.csrf().disable();
+		http.addFilterAfter(new RequestHeaderAuthenticationDecorator(), BasicAuthenticationFilter.class);
+	}
+
+	@Bean
+	public Docket api() {
+		return new Docket(DocumentationType.SWAGGER_2)
+				.select()
+				.apis(RequestHandlerSelectors.any())
+				.paths(not(regex("/error")))
+				.build();
 	}
 
 }
