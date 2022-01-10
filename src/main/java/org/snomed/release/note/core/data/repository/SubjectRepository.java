13b5e19bfc85b2bbfcf644a816134ab431533d43
Elena Ilyukhina <eil@ihtsdo.org>
2022-01-10 20:26:48 +0000

    Updated SubjectController

--- src/main/java/org/snomed/release/note/core/data/repository/SubjectRepository.java
+++ src/main/java/org/snomed/release/note/core/data/repository/SubjectRepository.java
@@ -7,6 +7,8 @@ import java.util.List;
 
 public interface SubjectRepository extends ElasticsearchRepository<Subject, String> {
 
+	List<Subject> findByPath(String path);
+
 	List<Subject> findByTitle(String title);
 
 }
