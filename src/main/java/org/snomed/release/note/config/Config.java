1f9df6ba25238ecf9731345e49930e2994c279ae
Elena Ilyukhina <eil@ihtsdo.org>
2022-01-17 20:49:01 +0000

    Enabled Elasticsearch repositories

--- src/main/java/org/snomed/release/note/config/Config.java
+++ src/main/java/org/snomed/release/note/config/Config.java
@@ -3,8 +3,13 @@ package org.snomed.release.note.config;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.context.annotation.PropertySource;
+import org.springframework.data.elasticsearch.repository.config.EnableElasticsearchRepositories;
 
 @SpringBootApplication
+@EnableElasticsearchRepositories(
+		basePackages = {
+				"org.snomed.release.note.core.data.repository"
+		})
 @EnableConfigurationProperties
 @PropertySource(value = "classpath:application.properties", encoding = "UTF-8")
 public abstract class Config extends ElasticsearchConfig {
