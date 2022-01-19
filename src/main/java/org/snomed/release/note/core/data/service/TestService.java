08f348af056e2181fb8ca29495f66ede036cef3b
Elena Ilyukhina <eil@ihtsdo.org>
2022-01-19 09:40:06 +0000

    Added test controller to generate test data

---
+++ src/main/java/org/snomed/release/note/core/data/service/TestService.java
@@ -0,0 +1,77 @@
+package org.snomed.release.note.core.data.service;
+
+import org.ihtsdo.otf.rest.exception.BadConfigurationException;
+import org.ihtsdo.otf.rest.exception.BusinessServiceException;
+import org.json.JSONArray;
+import org.json.JSONObject;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.snomed.release.note.core.data.domain.LineItem;
+import org.snomed.release.note.core.data.domain.Subject;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.io.BufferedReader;
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+@Service
+public class TestService {
+
+	private static final Logger LOGGER = LoggerFactory.getLogger(TestService.class);
+
+	@Autowired
+	private SubjectService subjectService;
+
+	@Autowired
+	private LineItemService lineItemService;
+
+	public void createData(final String path) throws BusinessServiceException {
+		try {
+			JSONArray array = new JSONArray(readFile("src/test/resources/data.json"));
+			createLineItems(array, path, null);
+		} catch (IOException e) {
+			LOGGER.error(e.getMessage());
+			throw new BadConfigurationException(e.getMessage(), e);
+		}
+	}
+
+	public void deleteData(final String path) {
+		subjectService.find(path).forEach(subject -> subjectService.delete(subject.getId(), path));
+		lineItemService.find(path).forEach(lineItem -> lineItemService.delete(lineItem.getId(), path));
+	}
+
+	private String readFile(final String fileName) throws IOException {
+		StringBuilder sb = new StringBuilder();
+		try (BufferedReader br = new BufferedReader(new InputStreamReader(new FileInputStream(fileName)))) {
+			br.lines().forEach(line -> sb.append(line));
+		}
+		return sb.toString();
+	}
+
+	private Subject createSubject(final JSONObject obj, final String path) throws BusinessServiceException {
+		return subjectService.create(new Subject(obj.getString("title"), path), path);
+	}
+
+	private LineItem createLineItem(final JSONObject obj, final String path, final String parentId) throws BusinessServiceException {
+		Subject subject = createSubject(obj, path);
+		String content = null;
+		if (obj.get("content") != JSONObject.NULL) {
+			content = obj.getString("content");
+		}
+		LineItem lineItem = new LineItem(subject.getId(), content, path);
+		lineItem.setLevel(obj.getInt("level"));
+		lineItem.setSequence(obj.getInt("sequence"));
+		lineItem.setParentId(parentId);
+		return lineItemService.create(lineItem, path);
+	}
+
+	private void createLineItems(JSONArray array, String path, String parentId) throws BusinessServiceException {
+		for (int i = 0; i < array.length(); i++) {
+			JSONObject obj = array.getJSONObject(i);
+			LineItem lineItem = createLineItem(obj, path, parentId);
+			createLineItems(obj.getJSONArray("children"), path, lineItem.getId());
+		}
+	}
+}
---
+++ src/test/java/org/snomed/release/note/core/data/service/TestServiceTest.java
@@ -0,0 +1,37 @@
+package org.snomed.release.note.core.data.service;
+
+import org.ihtsdo.otf.rest.exception.BusinessServiceException;
+import org.junit.jupiter.api.Test;
+import org.snomed.release.note.AbstractTest;
+import org.snomed.release.note.core.data.domain.LineItem;
+import org.snomed.release.note.core.data.domain.Subject;
+import org.springframework.beans.factory.annotation.Autowired;
+
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+public class TestServiceTest extends AbstractTest {
+
+	@Autowired
+	private TestService testService;
+
+	@Test
+	void testCreateData() throws BusinessServiceException {
+		testService.createData("MAIN/Test");
+		List<Subject> subjects = subjectService.findAll();
+		List<LineItem> lineItems = lineItemService.findAll();
+		assertEquals(7, subjects.size());
+		assertEquals(7, lineItems.size());
+	}
+
+	@Test
+	void testDeleteData() throws BusinessServiceException {
+		testService.createData("MAIN");
+		testService.deleteData("MAIN");
+		List<Subject> subjects = subjectService.findAll();
+		List<LineItem> lineItems = lineItemService.findAll();
+		assertEquals(0, subjects.size());
+		assertEquals(0, lineItems.size());
+	}
+}
