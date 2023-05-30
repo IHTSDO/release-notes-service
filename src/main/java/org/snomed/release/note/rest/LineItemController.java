f1a7add4019a7d17247f6cc55716d9bd1f0e5d2b
Elena Ilyukhina <eil@ihtsdo.org>
2023-05-30 13:04:20 +0100

    FRI-654 - Updated permissions for /version endpoint

--- src/main/java/org/snomed/release/note/rest/LineItemController.java
+++ src/main/java/org/snomed/release/note/rest/LineItemController.java
@@ -89,7 +89,7 @@ public class LineItemController {
 	}
 
 	@PostMapping(value = "/{path}/lineitems/version")
-	@PreAuthorize("hasPermission('RELEASE_ADMIN', #path) || hasPermission('RELEASE_MANAGER', #path)")
+	@PreAuthorize("hasPermission('PROJECT_MANAGER', #path) || hasPermission('RELEASE_ADMIN', #path) || hasPermission('RELEASE_MANAGER', #path)")
 	@ApiOperation(value = "Version line items", notes = "Move all line items from the code system branch 'path' to a release branch 'path/effectiveTime', e.g. 'MAIN/2022-01-31'")
 	public ResponseEntity<String> versionLineItem(
 			@PathVariable String path,
