29a00f460fd13b0a824fd3eb93b0023f868fbc78
Elena Ilyukhina <eil@ihtsdo.org>
2023-11-10 15:56:56 +0000

    PIP-378 - Added workaround for encoded slash after tomcat 10 upgrade

--- src/main/java/org/snomed/release/note/Application.java
+++ src/main/java/org/snomed/release/note/Application.java
@@ -1,13 +1,21 @@
 package org.snomed.release.note;
 
+import org.apache.tomcat.util.buf.EncodedSolidusHandling;
 import org.snomed.release.note.config.Config;
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.web.embedded.tomcat.TomcatConnectorCustomizer;
+import org.springframework.context.annotation.Bean;
 
 public class Application extends Config {
 
 	public static void main(String[] args) {
-		System.setProperty("org.apache.tomcat.util.buf.UDecoder.ALLOW_ENCODED_SLASH", "true"); // Swagger encodes the slash in branch paths
 		SpringApplication.run(Application.class, args);
+	}
+
+	@Bean
+	public TomcatConnectorCustomizer connectorCustomizer() {
+		// Swagger encodes the slash in branch paths
+		return connector -> connector.setEncodedSolidusHandling(EncodedSolidusHandling.DECODE.getValue());
 	}
 
 }
